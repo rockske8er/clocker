b288b4580dea6d298e48251e02cee110ae9782a2
Marcos de Souza <markus90souza@pop-os.localdomain>
2021-04-09 18:19:27 -0300

    Feature: Add login and SignUp with Firebase Auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,72 +1,77 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
 import { useFormik } from 'formik'
 import * as yup from 'yup'
 
 import {
     Container,
     Box,
+    Input,
+    Button,
+    Text,
     FormControl,
     FormLabel,
     FormHelperText,
-    Text,
-    Input,
     InputGroup,
     InputRightElement,
-    InputLeftElement,
-    Button,
-    InputLeftAddon
+    InputLeftElement
  } from '@chakra-ui/react'
 
  import { Icon } from '@chakra-ui/icons'
  import { FiEye, FiEyeOff, FiLock, FiMail } from 'react-icons/fi'
 
-import { Logo } from './../components'
+import { Logo } from '../components'
+import firebase from '../config/Firebase'
 
 const validationSchema = yup.object().shape({
     email: yup.string().email('E-mail inválido').required('Preenchimento obrigatório'),
     password: yup.string().required('Preenchimento obrigatório'),
 })
 
-export default function Home() {
+export default function Login() {
 
-const {
-    isSubmitting,
-    values,
-    handleBlur,
-    handleChange,
-    handleSubmit,
-    errors,
-    touched
-} = useFormik({
-    initialValues: {
-        email: '',
-        password: '',
-        username: ''
-    },
+    const router = useRouter()
 
-    onSubmit: (value) => {
-        console.log(value);
-    },
-    validationSchema
+    const {
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting
+    } = useFormik({
+        onSubmit: async ({email , password }) => {
+            try{
+                const userData =  await firebase.auth()
+                .signInWithEmailAndPassword(email, password);
+                console.log(userData)
+            }catch(e){
+                console.log('Error', e)
+            }
 
-
-})
+        },
+        validationSchema,
+        initialValues: {
+            email: '',
+            password: ''
+        }
+    })
 
     const [isVisible, setIsVisible] = useState(false);
 
     const handleClick = () => setIsVisible(!isVisible);
-  return (
-      <Container mt={10} centerContent>
-          <Logo />
 
-          <Box mt={'8'} mb={'8'}>
-            <Text>Crie sua agenda compartilhada</Text>
-          </Box>
+    return (
+        <Container  centerContent>
+            <Logo />
+            <Box mb={8} mt={8}>
+                <Text>Crie sua agenda compartilhada</Text>
+            </Box>
 
-          <Box>
-
-            <form onSubmit={handleSubmit}>
-
+            <Box>
+                <form onSubmit={handleSubmit}>
 
 
             <FormControl id="email" mt={'8'} isRequired>
@@ -93,7 +98,7 @@ const {
 
             </FormControl>
 
-            <FormControl id="password" mt={'8'}>
+                <FormControl id="password" mt={'8'}>
                 <FormLabel>Senha</FormLabel>
                 <InputGroup alignItems="center">
                     <InputLeftElement>
@@ -126,37 +131,17 @@ const {
 
             </FormControl>
 
-            <FormControl id="username" mt={'8'} >
-                <InputGroup  size={'lg'}>
-                    <InputLeftAddon children={'clocker.work/'} />
-                    <Input
-                        name="username"
-                        size={'lg'}
-                        type="username"
-                        placeholder="username"
-                        value={values.username}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                    />
-                </InputGroup>
 
-                {
-                    touched.username && <FormHelperText textColor={'red.400'}>{errors.username}</FormHelperText>
-                }
+                    <Button
+                    colorScheme="blue"
+                    width="100%"
+                    type="submit"
+                    isLoading={isSubmitting}>Entrar</Button>
+  </form>
+            </Box>
 
-                </FormControl>
-            <Button
-                type={'submit'}
-                mt="8" w={'full'} size={'lg'} colorScheme='blue'
-                isLoading={isSubmitting}
-
-                >
-                Entrar
-            </Button>
-            </form>
-          </Box>
-
-
-      </Container>
-  )
+            <Link href="/signup">Ainda não tem uma conta? Cadastre-se</Link>
+        </Container>
+    )
 }
+
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,7 +1,172 @@
-function Signup() {
-  return (
-    <h1>Signup</h1>
-  );
-}
+import { useState } from 'react'
+import { useFormik } from 'formik'
+import * as yup from 'yup'
 
-export default Signup;
+import {
+    Container,
+    Box,
+    FormControl,
+    FormLabel,
+    FormHelperText,
+    Text,
+    Input,
+    InputGroup,
+    InputRightElement,
+    InputLeftElement,
+    Button,
+    InputLeftAddon
+ } from '@chakra-ui/react'
+
+ import { Icon } from '@chakra-ui/icons'
+ import { FiEye, FiEyeOff, FiLock, FiMail } from 'react-icons/fi'
+
+import { Logo } from '../components'
+
+const validationSchema = yup.object().shape({
+    email: yup.string().email('E-mail inválido').required('Preenchimento obrigatório'),
+    password: yup.string().required('Preenchimento obrigatório'),
+    username: yup.string().required('Preenchimento obrigatório'),
+})
+
+import firebase from '../config/Firebase'
+
+export default function Home() {
+
+const {
+    isSubmitting,
+    values,
+    handleBlur,
+    handleChange,
+    handleSubmit,
+    errors,
+    touched
+} = useFormik({
+    initialValues: {
+        email: '',
+        password: '',
+        username: ''
+    },
+    validationSchema,
+    onSubmit: async ({email , password, username}) => {
+        try{
+            const userData =  await firebase.auth()
+            .createUserWithEmailAndPassword(email, password);
+            console.log(userData)
+        }catch(e){
+            console.log('Error', e)
+        }
+
+    }
+
+
+
+})
+
+    const [isVisible, setIsVisible] = useState(false);
+
+    const handleClick = () => setIsVisible(!isVisible);
+  return (
+      <Container mt={10} centerContent>
+          <Logo />
+
+          <Box mt={'8'} mb={'8'}>
+            <Text>Crie sua agenda compartilhada</Text>
+          </Box>
+
+          <Box>
+
+            <form onSubmit={handleSubmit}>
+
+
+
+            <FormControl id="email" mt={'8'} isRequired>
+                <FormLabel>E - mail</FormLabel>
+                <InputGroup>
+                    <InputLeftElement>
+                        <Icon as={FiMail} />
+                    </InputLeftElement>
+                    <Input
+                        name="email"
+                        size={'lg'}
+                        type="email"
+                        placeholder="seu@email.com"
+                        value={values.email}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                    />
+                </InputGroup>
+
+                {
+                    touched.email && <FormHelperText textColor={'red.400'}>{errors.email}</FormHelperText>
+                }
+
+
+            </FormControl>
+
+            <FormControl id="password" mt={'8'}>
+                <FormLabel>Senha</FormLabel>
+                <InputGroup alignItems="center">
+                    <InputLeftElement>
+                        <Icon as={FiLock} />
+                    </InputLeftElement>
+                    <Input
+                        name="password"
+                        size={'lg'}
+                        type={isVisible ? "text" : "password"}
+                        placeholder="Sua senha segura"
+                        value={values.password}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                    />
+                    <InputRightElement>
+
+                        {
+                            isVisible ? <Icon onClick={handleClick} as={FiEyeOff} />
+                                    : <Icon onClick={handleClick} as={FiEye} />
+                        }
+
+
+                    </InputRightElement>
+                </InputGroup>
+
+                {
+                    touched.password && <FormHelperText textColor={'red.400'}>{errors.password}</FormHelperText>
+                }
+
+
+            </FormControl>
+
+            <FormControl id="username" mt={'8'} >
+                <InputGroup  size={'lg'}>
+                    <InputLeftAddon children={'clocker.work/'} />
+                    <Input
+                        name="username"
+                        size={'lg'}
+                        type="username"
+                        placeholder="username"
+                        value={values.username}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                    />
+                </InputGroup>
+
+                {
+                    touched.username && <FormHelperText textColor={'red.400'}>{errors.username}</FormHelperText>
+                }
+
+                </FormControl>
+            <Button
+                type={'submit'}
+                mt="8" w={'full'} size={'lg'} colorScheme='blue'
+                isLoading={isSubmitting}
+
+                >
+                Entrar
+            </Button>
+            </form>
+          </Box>
+
+
+      </Container>
+  )
+}
